90318f8e7a9bcee42275ae1f7f1cdaccc085d87a
IloveKanade <xjlgxlgx@gmail.com>
2021-08-05 11:45:39 +0800

    new-feature:add k3fmt

---
+++ test/page_it.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2
+# coding: utf-8
+
+import sys
+
+import k3fmt
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+
+    pager = args.pop(0).split()
+    control_char = args.pop(0) == '1'
+    limit = int(args.pop(0))
+    lines = args
+
+    k3fmt.page(lines, max_lines=limit, pager=pager)
